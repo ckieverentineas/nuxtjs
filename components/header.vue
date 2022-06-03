78a4cd4800ebadd7a1fc7fec506833e9f15a5728
ckieverentineas <ckieverentineas@yandex.ru>
2022-06-03 11:08:13 +1000

    complete header

--- components/header.vue
+++ components/header.vue
@@ -3,9 +3,9 @@
   <div id="main">
     <div id="nav">
       <img id="logo" src="favicon.ico"/>
-      <a id="order"> Каталог</a>
-      <a id="about"> О нас</a>
-      <a id="contact"> Контакты</a>
+      <div> Каталог </div>
+      <div> О нас </div>
+      <div> Контакты </div>
       <img id="cart" src="cart.png"/>
     </div>
   </div>
@@ -42,100 +42,32 @@ export default {
 
 <style>
 div { 
-  display: flex;
-}
-#main {
-  text-align: center;
-  justify-content: center;
-  vertical-align: middle;
+  width: 100%;
   margin-left: auto;
   margin-right: auto;
   display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  vertical-align: middle;
 }
+
 #nav {
   /* nav/desktop */
-  position: absolute;
-  width: 1440px;
-  height: 75px;
-  left: 0px;
+  width: 100%;
   top: 0px;
   background: #d3eff0;
   display: inline-flex; 
 }
 #logo {
   /* logo */
-position: absolute;
-width: 100px;
-height: 50px;
-left: 165px;
-top: 20px;
-}
-#order {
-  /* Discovery */
-  position: absolute;
-  width: 85px;
-  height: 25px;
-  left: 533px;
-  top: 25px;
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 19px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  color: #272727;
-}
-#about {
-  /* About */
-position: absolute;
-width: 129px;
-height: 25px;
-left: 655px;
-top: 25px;
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 19px;
-display: flex;
-align-items: center;
-text-align: center;
-color: #272727;
+  width: 100px;
+  height: 50px;
 }
 
-#contact {
-  /* Contact us */
-
-
-position: absolute;
-width: 130px;
-height: 25px;
-left: 790px;
-top: 25px;
-
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 19px;
-display: flex;
-align-items: center;
-text-align: center;
-
-/* black */
-
-color: #272727;
-}
 #cart {
   /* Cart */
-
-
-position: absolute;
 width: 26px;
 height: 26px;
-left: 1244px;
-top: 24px;
 }
 </style>
